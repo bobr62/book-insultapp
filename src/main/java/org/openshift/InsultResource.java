69a90528175de30ff7e519af4c10561e483ddfbb
Bob Richmond <bob.richmond@palisadesolutions.com>
2017-08-24 11:18:28 -0600

    fixed theInsult() to theInsult

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -16,6 +16,6 @@ public class InsultResource {
 	public HashMap<String,String> getInsult() {
 		HashMap<String, String> theInsult = new HashMap<String, String>();
 		theInsult.put("insult", new InsultGenerator().generateInsult());
-		return theInsult();
+		return theInsult;
 	}
 }
